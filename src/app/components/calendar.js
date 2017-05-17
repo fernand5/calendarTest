43345ea2516742496141043bb5849f375d07c51c
Grupo Medicus <git@grupomedicus.com>
2017-05-16 21:17:27 -0400

    Css edited

--- src/app/components/calendar.js
+++ src/app/components/calendar.js
@@ -11,26 +11,23 @@ angular
     }
   });
 /** @ngInject */
-function CalendarController($http, $compile, holydayService,$resource) {
+function CalendarController($http, $compile, holydayService, $resource) {
 
 
-  var vm= this;
-
-  // vm.isHoliday=function(day){
-  //   console.log("alo");
-  //   // day['class']='holiday';
-  //   var User = $resource('https://holidayapi.com/v1/holidays');
-  //   User.get({
-  //     year:day['year'],
-  //     country:'US',
-  //     key:'12ed68aa-eebb-4c0b-a37b-6d3202de35ef',
-  //     day:day['number']
-  //     ,month:day['month']}).$promise.then(
-  //     function(data){
-  //       console.log(data);
-  //     },
-  //     function(reason){
-  //       console.log(reason);
-  //     });
-  // }
+  var vm = this;
+  vm.daysOfTheWeek = [{
+    'name': 'Sun'
+  }, {
+    'name': 'Mon'
+  }, {
+    'name': 'Tue'
+  }, {
+    'name': 'Wed'
+  }, {
+    'name': 'Thu'
+  }, {
+    'name': 'Fri'
+  }, {
+    'name': 'Sat'
+  }];
 }
